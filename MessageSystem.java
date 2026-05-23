f9c82111869c3c6a6bdde7e549de66d8da7d86a9
masindib <ST10491611@rcconnect.edu.za>
2026-05-23 08:22:36 +0200

    Remove MessageSystem class and its content

--- MessageSystem.java
+++ MessageSystem.java
@@ -1,16 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-package com.mycompany.messagesystem;
-
-/**
- *
- * @author mbedz
- */
-public class MessageSystem {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
